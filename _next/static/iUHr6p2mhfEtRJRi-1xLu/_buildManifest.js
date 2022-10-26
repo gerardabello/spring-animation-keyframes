266f3785a9ec2f4f5b34c850919eeb16ba2185e3
gerardabello <gerardabello@users.noreply.github.com>
2022-10-26 13:39:40 +0000

    deploy: fa9290fcf18a8ac56307fe30bd9f17d78189782c

---
+++ _next/static/iUHr6p2mhfEtRJRi-1xLu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5a998bf950233a1e.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
