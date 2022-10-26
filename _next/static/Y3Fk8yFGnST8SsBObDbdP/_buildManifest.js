c2d8c87ae23629aa5f8debe46dc1ea0e53f37f35
gerardabello <gerardabello@users.noreply.github.com>
2022-10-26 20:34:13 +0000

    deploy: c21542d4299426a0b4b79fa1f65c27b0148a080b

---
+++ _next/static/Y3Fk8yFGnST8SsBObDbdP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/321-e59e2b30f04cd177.js","static/chunks/pages/index-6b3a0cfbd1aaea1a.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/iUHr6p2mhfEtRJRi-1xLu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5a998bf950233a1e.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
